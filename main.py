61e68d007257c4ec3629281f4ac04cc04e181da3
hunght1890 <hungsaki2003@gmail.com>
2023-05-30 08:53:41 +0700

    update source

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ system('title Open Code Quickly - Python - t.me/hunght1890')
 def open_folder_code():
     path = input("Path folder: ")
     if path != '':
-        system(f'cd {path} & code . & exit')
+        system(f'cd /d {path} & code . & exit')
     else:
         system('cls')
         return open_folder_code()
